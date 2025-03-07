92d113a3658b90e77aa1a00b2468b9ccebe81a79
Ben Swift <ben@benswift.me>
2025-03-07 16:06:56 +1100

    add dummy tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -29,3 +29,33 @@ def test_moondream_i2t():
 
     assert isinstance(caption, str)
     assert len(caption) > 0  # Caption should not be empty
+
+def test_dummy_i2t():
+    """Test that dummy_i2t returns a fixed text caption."""
+    from src.models import IMAGE_SIZE, dummy_i2t
+
+    # Create a test image
+    image = Image.new('RGB', (IMAGE_SIZE, IMAGE_SIZE), color='blue')
+
+    caption = dummy_i2t(image)
+
+    assert isinstance(caption, str)
+    assert caption == "dummy text caption"
+
+
+def test_dummy_t2i():
+    """Test that dummy_t2i returns a fixed blank image with correct dimensions."""
+    from src.models import IMAGE_SIZE, dummy_t2i
+
+    prompt = "This prompt will be ignored"
+    image = dummy_t2i(prompt)
+
+    assert isinstance(image, Image.Image)
+    assert image.width == IMAGE_SIZE
+    assert image.height == IMAGE_SIZE
+
+    # Check that the image is white (all pixels have RGB value of 255)
+    # Sample a few pixels to verify
+    pixels = list(image.getdata())
+    assert pixels[0] == (255, 255, 255)  # Check top-left pixel
+    assert pixels[-1] == (255, 255, 255)  # Check bottom-right pixel
