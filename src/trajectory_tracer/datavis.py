45aff796291b65945081b2f245675c642a4f8742
Ben Swift <ben@benswift.me>
2025-04-09 21:15:19 +1000

    clean up datavis module again

--- src/trajectory_tracer/datavis.py
+++ src/trajectory_tracer/datavis.py
@@ -24,8 +24,6 @@ from plotnine import (
 from plotnine.options import set_option
 from sqlmodel import Session
 
-from trajectory_tracer.analysis import load_embeddings_df, load_runs_df
-
 ## datavis
 
 set_option("limitsize", False)
@@ -319,14 +317,29 @@ def paper_charts(session: Session) -> None:
     """
     # from trajectory_tracer.analysis import warm_caches
     # warm_caches(session)
-    embeddings_df = load_embeddings_df(session, use_cache=True)
-    plot_semantic_drift(embeddings_df, "output/vis/semantic_drift.png")
+    # embeddings_df = load_embeddings_df(session, use_cache=True)
+    # plot_semantic_drift(embeddings_df, "output/vis/semantic_drift.png")
+
+    from trajectory_tracer.analysis import load_runs_df
 
     runs_df = load_runs_df(session, use_cache=True)
-    plot_persistence_diagram_faceted(
-        runs_df, "output/vis/persistence_diagram_faceted.png"
-    )
-    plot_persistence_entropy(runs_df, "output/vis/persistence_entropy.png")
+
+    # Count rows for each combination of model types and homology dimension
+    model_counts = runs_df.group_by([
+        "text_model",
+        "image_model",
+        "embedding_model",
+        "homology_dimension",
+    ]).count()
+    print("Counts per model combination and homology dimension:")
+    model_counts.write_csv("out.csv")
+
+    # from trajectory_tracer.analysis import load_embeddings_df
+
+    # plot_persistence_diagram_faceted(
+    #     runs_df, "output/vis/persistence_diagram_faceted.png"
+    # )
+    # plot_persistence_entropy(runs_df, "output/vis/persistence_entropy.png")
     # plot_persistence_diagram_by_run(
     #     runs_df, 16, "output/vis/persistence_diagram_by_run.png"
     # )
