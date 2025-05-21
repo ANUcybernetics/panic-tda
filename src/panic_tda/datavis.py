b798cc5da3fb43ae2fa79354db50f80e867c1d64
Ben Swift <ben@benswift.me>
2025-05-21 16:52:49 +1000

    comment out some code (for now)

--- src/panic_tda/datavis.py
+++ src/panic_tda/datavis.py
@@ -619,13 +619,14 @@ def plot_cluster_bubblegrid(
         .alias("display_label")
     )
 
-    print(
-        counts_df.filter(pl.col("display_label") != pl.lit(""))
-        .select("cluster_index", "cluster_label")
-        .unique()
-        .to_pandas()
-        .to_latex(index=False)
-    )
+    # print out all the "labelled" (i.e. top 10%) cluster labels to LaTeX
+    # print(
+    #     counts_df.filter(pl.col("display_label") != pl.lit(""))
+    #     .select("cluster_index", "cluster_label")
+    #     .unique()
+    #     .to_pandas()
+    #     .to_latex(index=False)
+    # )
 
     # Convert to pandas for plotting (only at the end)
     pandas_df = counts_df.to_pandas()
