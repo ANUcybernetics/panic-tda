a6b00561154717eac7153d7e1298f1fad65b37d1
Ben Swift <ben@benswift.me>
2025-07-16 13:16:20 +1000

    add alembic for db migrations

---
+++ alembic/versions/initial_initial_schema.py
@@ -0,0 +1,30 @@
+"""Initial schema
+
+Revision ID: initial
+Revises: 
+Create Date: 2025-07-16 13:07:03.103438
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'initial'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    # Initial migration - database already exists
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    # Initial migration - nothing to downgrade
+    pass
